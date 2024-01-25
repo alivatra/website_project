e9c03c075c3aedce608b62e95a6085217ca7cd1c
alivatra <140677880+alivatra@users.noreply.github.com>
2024-01-25 11:05:07 +0700

    Add files via upload

---
+++ pertemuan 3/operaetor_aritmatika.py
@@ -0,0 +1,10 @@
+x = 5 
+y = 3
+
+print("penjumlahan = ", x + y)
+print("pengurangan = ", x - y)
+print("perkalian = ", x * y)
+print("pembagian = ", x / y)
+print("perpangkatan  = ", x ** y)
+print("modulus= ", x % y)
+print("pembagian bulat = ", x // y)
---
+++ pertemuan 3/operator_penugasan.py
@@ -0,0 +1,19 @@
+a = 5
+b = 6
+c = 7
+d = 4
+e = 16
+f = 2
+ 
+a +=3
+print("penjumlahan a =", a)
+b -= 4
+print("pengurangan b =", b)
+c *=  2
+print("perkalian c =", c)
+d /= 2
+print("pembagian d =", d)
+e %= 3
+print("modulus e =", e)
+f **= 1000
+print("pangkat =", f)
