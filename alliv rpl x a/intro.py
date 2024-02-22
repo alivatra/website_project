7a4b1f58425af7d442820171964286bbb4dc2085
alivatra <140677880+alivatra@users.noreply.github.com>
2024-02-21 20:15:37 -0800

    Add files via upload

---
+++ alliv rpl x a/intro.py
@@ -0,0 +1,8 @@
+def pembukaan() :
+    print("Selamat datang di educourse")
+
+def perkenalan(nama, kelas):
+    print("nama saya", nama, "dan saya kelas", kelas)
+
+pembukaan()
+perkenalan("alliv", "RPL X A")
